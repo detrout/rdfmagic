68c4ed97c6be2044fe1ac8cfba57dc896a6eb81b
Diane Trout <diane@ghic.org>
2014-02-06 12:01:15 -0800

    Put project under LGPL 2.1+ license

--- setup.py
+++ setup.py
@@ -1,3 +1,19 @@
+# Copyright (C) 2014 Diane Trout
+#
+# This package is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License Version 2.1
+# as published by the Free Software Foundation or any newer version.
+
+# This package is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+# License Version 2.1 for more details.
+
+# You should have received a copy of the GNU Lesser General Public
+# License Version 2.1 along with this package; if not, write to the
+# Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
+# MA 02110-1301 USA
+
 from setuptools import setup
 
 setup(
