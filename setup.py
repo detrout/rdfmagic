75044cf9aad499a396bb04c7e142ec2bf580dede
Diane Trout <diane@ghic.org>
2015-05-27 15:22:15 -0700

    flake8 whitespace cleanup

--- setup.py
+++ setup.py
@@ -17,12 +17,13 @@
 from setuptools import setup
 
 setup(
-  name='rdfmagic',
-  version='0.1',
-  description='IPython magic functions to make it easier to query Redland librdf triple stores',
-  author='Diane Trout',
-  author_email='diane@ghic.org',
-  packages=['rdfmagic'],
-  install_requires=['IPython >= 0.12'],
-  test_suite='test.test_rdfmagic.suite'
+    name='rdfmagic',
+    version='0.1',
+    description='IPython magic functions to make it easier'
+                'to query Redland librdf triple stores',
+    author='Diane Trout',
+    author_email='diane@ghic.org',
+    packages=['rdfmagic'],
+    install_requires=['IPython >= 0.12'],
+    test_suite='rdfmagic.test.test_rdfmagic.suite'
 )
